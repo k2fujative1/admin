af31fd0f95a0c64b534383f75a8835c9758532ba
admin <admin@admin.com>
2018-08-19 20:27:44 -0400

    fixed ps.sh to bin/sh

--- ps.sh
+++ ps.sh
@@ -1,2 +1,12 @@
-#!/bin/bash
-ps
+#!/bin/sh
+echo -e "Content-type: text/html\n"
+echo "<html><body"
+echo "<h1>top</h1>"
+echo "<pre>"
+top -b -n 1
+echo "</pre>"
+echo "<h1>ps</h1>"
+echo "<pre>"
+ps aux
+echo "</pre>"
+echo "</body></html>"
